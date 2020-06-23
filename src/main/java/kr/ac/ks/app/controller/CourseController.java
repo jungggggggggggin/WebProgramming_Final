9620abe27e1dac30d2fc14bc9b46043c5d15e2c5
jugggggggggggin <wjddls990313@naver.com>
2020-06-23 23:02:26 +0900

    기말과제 - 수정과 삭제 기능 구현

---
+++ src/main/java/kr/ac/ks/app/controller/CourseController.java
@@ -0,0 +1,83 @@
+package kr.ac.ks.app.controller;
+
+import kr.ac.ks.app.domain.Course;
+import kr.ac.ks.app.domain.Lesson;
+import kr.ac.ks.app.domain.Student;
+import kr.ac.ks.app.repository.CourseRepository;
+import kr.ac.ks.app.repository.LessonRepository;
+import kr.ac.ks.app.repository.StudentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class CourseController {
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+    private final LessonRepository lessonRepository;
+
+    public CourseController(StudentRepository studentRepository, CourseRepository courseRepository, LessonRepository lessonRepository) {
+        this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @GetMapping("/course")
+    public String showCourseForm(Model model) {
+        List<Student> students = studentRepository.findAll();
+        List<Lesson> lessons = lessonRepository.findAll();
+        model.addAttribute("students", students);
+        model.addAttribute("lessons", lessons);
+        return "courses/courseForm";
+    }
+
+    @PostMapping("/course")
+    public String createCourse(@RequestParam("studentId") Long studentId,
+                               @RequestParam("lessonId") Long lessonId
+                               ) {
+        Student student = studentRepository.findById(studentId).get();
+        Lesson lesson = lessonRepository.findById(lessonId).get();
+        Course course = Course.createCourse(student,lesson);
+        Course savedCourse = courseRepository.save(course);
+        return "redirect:/courses";
+    }
+
+    @GetMapping("/courses")
+    public String courseList(Model model) {
+        List<Course> courses = courseRepository.findAll();
+        model.addAttribute("courses", courses);
+        return "courses/courseList";
+    }
+
+    @GetMapping("course/update/{id}")
+    public String showUpdateCourseForm(@PathVariable("id") Long id, Model model) {
+        Lesson lesson = lessonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        model.addAttribute("student", student);
+        model.addAttribute("lesson", lesson);
+        return "lessons/lessonUpdate";
+    }
+
+    @PostMapping("course/update/{id}")
+    public String updateCourse(Course course) {
+        courseRepository.save(course);
+        return "redirect:/course";
+    }
+
+    @GetMapping("course/delete/{id}")
+    public String deleteCourse(@PathVariable("id") Long id, Model model) {
+        Lesson lesson = lessonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        studentRepository.delete(student);
+        lessonRepository.delete(lesson);
+        model.addAttribute("lessons", lessonRepository.findAll());
+        model.addAttribute("students", studentRepository.findAll());
+        return "redirect:/lessons";
+    }
+
+}
---
+++ src/main/java/kr/ac/ks/app/controller/LessonController.java
@@ -0,0 +1,66 @@
+package kr.ac.ks.app.controller;
+
+import kr.ac.ks.app.domain.Lesson;
+import kr.ac.ks.app.domain.Student;
+import kr.ac.ks.app.repository.LessonRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class LessonController {
+
+    private final LessonRepository lessonRepository;
+
+    public LessonController(LessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
+    @GetMapping(value = "/lessons/new")
+    public String createForm(Model model) {
+        model.addAttribute("lessonForm", new LessonForm());
+        return "lessons/lessonForm";
+    }
+
+    @PostMapping(value = "/lessons/new")
+    public String create(LessonForm form) {
+        Lesson lesson = new Lesson();
+        lesson.setName(form.getName());
+        lesson.setQuota(form.getQuota());
+        lessonRepository.save(lesson);
+        return "redirect:/lessons";
+    }
+
+    @GetMapping(value = "/lessons")
+    public String list(Model model) {
+        List<Lesson> lessons = lessonRepository.findAll();
+        model.addAttribute("lessons", lessons);
+        return "lessons/lessonList";
+    }
+
+    @GetMapping("lesson/update/{id}")
+    public String showUpdateLessonForm(@PathVariable("id") Long id, Model model) {
+        Lesson lesson = lessonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        model.addAttribute("lesson", lesson);
+        return "lessons/lessonUpdate";
+    }
+
+    @PostMapping("lesson/update/{id}")
+    public String updateLesson(Lesson lesson) {
+        lessonRepository.save(lesson);
+        return "redirect:/lessons";
+    }
+
+    @GetMapping("lesson/delete/{id}")
+    public String deleteLesson(@PathVariable("id") Long id, Model model) {
+        Lesson lesson = lessonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        lessonRepository.delete(lesson);
+        model.addAttribute("lessons", lessonRepository.findAll());
+        return "redirect:/lessons";
+    }
+
+}
